8f1931a5fd8ce25903011533ad12f8ec80772d68
Juan Pablo Mejía <juan.mejiab@upb.edu.co>
2020-09-22 21:12:13 -0500

    2

--- Bandera.java
+++ Bandera.java
@@ -6,17 +6,16 @@ public class Bandera{
 
 		for(int i=1;i<341;i++){
 			if(i%34==0){
-				System.out.print(bandera[0][0]);
 				System.out.println();
 			}
 			else if(i<=102){
-				System.out.print("\033[0;31m" + bandera[0][0]);
+				System.out.print("\033[0;31m" + "\033[41m" + bandera[0][0] + "\033[0m");
 				}
 			else if(i>102 && i<=238){
-				System.out.print("\033[0;33m" + bandera[0][0]);
+				System.out.print("\033[0;33m" + "\033[43m" + bandera[0][0] + "\033[0m");
 			}
 			else if(i>238){
-				System.out.print("\033[0;31m" + bandera[0][0]);
+				System.out.print("\033[0;31m" + "\033[41m" + bandera[0][0] + "\033[0m");
 				}
 		}
 	}
